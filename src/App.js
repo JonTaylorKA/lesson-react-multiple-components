bcbb2002eafd0d20c9d679834769f78c810f5509
Jon Taylor <jontaylor224@gmail.com>
2020-03-02 12:05:44 -0600

    finished lesson

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome/welcome'
+import Clock from './components/clock/clock'
+import Contact from './components/contact/contact'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Welcome name='Eric' />
-        <h2>Welcome to React</h2>
+        <Clock />
+        <Contact />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
